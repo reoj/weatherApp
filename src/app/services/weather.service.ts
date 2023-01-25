6cc1ff32a770fd01a7293e9892c8209f02eb4bc4
Jaciel Israel Resendiz Ochoa <jresendizochoa@deloitte.com>
2023-01-25 14:24:49 -0600

    Implements Weather Service

---
+++ src/app/services/weather.service.ts
@@ -0,0 +1,65 @@
+import { SnackbarcontrolService } from './snackbarcontrol.service';
+import { Injectable, Input } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { ServerResponse } from '../models/weatherReport.model';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class WeatherService {
+  public lastResult: ServerResponse = {} as ServerResponse;
+  public hasWeather = false;
+  public loading = false;
+  public isReadyForUpdate = false;
+
+  constructor(public client: HttpClient, public snackBar: SnackbarcontrolService) {
+    this.client = client;
+  }
+
+  public getWeather(): ServerResponse {
+    return this.lastResult;
+  }
+
+  public async fetchWeather(city: string) {
+    this.loading = true;
+    this.isReadyForUpdate = false;
+    var url = `https://wttr.in/${city}?format=j2`;
+    this.client.get<ServerResponse>(url).subscribe({
+      next: (data: ServerResponse) => {
+        this.handleResponse(data);
+      },
+      error: (err) => {
+        var message = 'Error fetching weather data:' + err.message;
+        this.snackBar.openSnackBar(message, 'OK');
+        this.loading = false;
+        this.isReadyForUpdate = false;
+      },
+    });
+  }
+
+  handleResponse(data: ServerResponse) {
+    if (data.weather == undefined) {
+      this.lastResult = {} as ServerResponse;
+      return;
+    }
+    //console.log(data);
+    this.lastResult = data;
+    this.update();
+    this.loading = false;
+    this.isReadyForUpdate = true;
+  }
+
+  public update() {
+    this.hasWeather = this.lastResult.nearest_area != undefined;
+  }
+  public getWeatherAvailable() {
+    return this.hasWeather;
+  }
+
+  clear() {
+    this.lastResult = {} as ServerResponse;
+    this.hasWeather = false;
+    this.loading = false;
+    this.isReadyForUpdate = false;
+  }
+}
