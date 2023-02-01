7e5ad6a2d03fb7c20893cba5345c72473d1eab47
Jaciel Israel Resendiz Ochoa <jresendizochoa@deloitte.com>
2023-02-01 12:51:52 -0600

    Implements display of forecast data in reusable components

--- src/app/weather-display/forecast-card/forecast-card.component.ts
+++ src/app/weather-display/forecast-card/forecast-card.component.ts
@@ -1,6 +1,6 @@
-import { WeatherDisplayComponent } from './../weather-display.component';
 import { Component, Input, OnInit } from '@angular/core';
 import { Weather } from 'src/app/models/weatherReport.model';
+import { WeatherToIconService } from 'src/app/services/weather-to-icon.service';
 
 @Component({
   selector: 'forecast-card',
@@ -8,32 +8,48 @@ import { Weather } from 'src/app/models/weatherReport.model';
   styleUrls: ['./forecast-card.component.css'],
 })
 export class ForecastCardComponent implements OnInit {
-  icons = ['rainy', 'cloudy', 'sunny', 'snowy'];
-  icon = 'rainy';
-  isHourly = false;
+  constructor(public iconService: WeatherToIconService) {}
+  date = '';
+  uvIndex = '';
+  maxTemp: string = '';
+  minTemp: string = '';
+  hourlyModels: {
+    time: string;
+    description: string;
+    icon: string;
+    temperature: string;
+  }[] = [];
+
   @Input() forecast: Weather = {} as Weather;
   @Input() title = 'Today';
 
   ngOnInit(): void {
-    this.isHourly = this.containsHourly(this.forecast);
-    if (this.isHourly) {
-      this.buildHourlyForecast(this.forecast);
-    } else {
-      this.buildGeneralForecast(this.forecast);
+    this.mapGeneralData();
+    var hours = this.forecast.hourly;
+    for (let i = 0; i < hours.length; i++) {
+      var description = hours[i].weatherDesc[0].value;
+      var hour = this.convertMilitaryTimeToHours(hours[i].time);
+      var foundIcon = this.iconService.getIcon(description);
+      var temperature = hours[i].tempC;
+      this.hourlyModels.push({
+        time: hour,
+        description: description,
+        icon: foundIcon,
+        temperature: temperature,
+      });
     }
-    this.displayResult();
   }
 
-  public containsHourly(forecast: Weather): boolean {
-    return forecast.hourly !== undefined;
+  mapGeneralData (){
+    this.maxTemp = this.forecast.maxtempC;
+    this.minTemp = this.forecast.mintempC;
+    this.uvIndex = this.forecast.uvIndex;
+    this.date = this.forecast.date;
   }
-
-  buildHourlyForecast(forecast: Weather) {
-    throw new Error('Function not implemented.');
-  }
-
-  buildGeneralForecast(forecast: Weather) {
-    throw new Error('Function not implemented.');
+  convertMilitaryTimeToHours(militaryTime: string) {
+    var military = parseInt(militaryTime);
+    var hours = Math.floor(military / 100);
+    return '' + hours;
   }
 
   displayResult() {
