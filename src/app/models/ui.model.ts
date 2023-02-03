f801031cfd2cac4d9b9db5aedb36d0340b782ce1
Jaciel Israel Resendiz Ochoa <jresendizochoa@deloitte.com>
2023-02-03 11:47:31 -0600

    Creates new interfaces for UI Models

---
+++ src/app/models/ui.model.ts
@@ -0,0 +1,44 @@
+export class HourlyUIModel {
+  time: string = '0';
+  description: string = 'clear';
+  icon: string = 'sunny';
+  temperature: string = '0';
+
+  constructor(
+    time: string,
+    temperature: string,
+    description: string,
+    icon: string
+  ) {
+    this.time = time;
+    this.temperature = temperature;
+    this.description = description;
+    this.icon = icon;
+  }
+}
+
+export class CurrentConditionUIModel {
+  city = 'City';
+  country = 'Country';
+  station = 'Station';
+  description = 'Description';
+  icon = 'partly_cloudy_day';
+  currentTemp = '0';
+  feelsLike = '0';
+  humidity = '0';
+  pressure = '0';
+  visibility = '0';
+  uvIndex = '0';
+  windDirection = 'XXX';
+  windSpeed = '0';
+  maxTemp = '0';
+  minTemp = '0';
+  precipitationLevel = '0';
+}
+
+export class ForecastCardUIModel {
+  date: string = '';
+  uvIndex: string = '';
+  maxTemp: string = '';
+  minTemp: string = '';
+}
--- src/app/models/weatherReport.model.ts
+++ src/app/models/weatherReport.model.ts
@@ -1,38 +1,40 @@
 export interface ServerResponse {
-  current_condition: {
-    temp_C: string;
-    weatherDesc: [{ value: string }];
-    FeelsLikeC: string;
-    humidity: string;
-    pressure: string;
-    visibility: string;
-    uvIndex: string;
-    winddir16Point: string;
-    windspeedKmph: string;
-    precipMM: string;
-  }[];
-  nearest_area: [
-    {
-      region: [{ value: string }];
-      country: [{ value: string }];
-      areaName: [{ value: string }];
-    }
-  ];
+  current_condition: CurrentConditionData[];
+  nearest_area: Area[];
   request: {};
   weather: [Weather];
 }
 
+export interface Area {
+  region: [{ value: string }];
+  country: [{ value: string }];
+  areaName: [{ value: string }];
+}
+
+export interface CurrentConditionData {
+  temp_C: string;
+  FeelsLikeC: string;
+  humidity: string;
+  pressure: string;
+  visibility: string;
+  uvIndex: string;
+  winddir16Point: string;
+  windspeedKmph: string;
+  precipMM: string;
+  weatherDesc: [{ value: string }];
+}
+
 export interface Weather {
-  hourly: [
-    {
-      weatherDesc: [{ value: string }];
-      tempC: string;
-      time: string;
-    }
-  ];
+  hourly: [Hourly];
   maxtempC: string;
   mintempC: string;
   date: string;
   avgtempC: string;
   uvIndex: string;
+}
+
+export interface Hourly {
+  weatherDesc: [{ value: string }];
+  tempC: string;
+  time: string;
 }
