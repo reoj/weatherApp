e215a133c1de49d55d6858076d23be9ac4a64ffa
Jaciel Israel Resendiz Ochoa <jresendizochoa@deloitte.com>
2023-02-03 11:45:32 -0600

    Packs the relavant data from WeatherDisplay into objects

--- src/app/weather-display/weather-display.component.ts
+++ src/app/weather-display/weather-display.component.ts
@@ -1,7 +1,8 @@
-import { Weather } from 'src/app/models/weatherReport.model';
+import { CurrentConditionUIModel } from './../models/ui.model';
+import { CurrentConditionData, Weather } from 'src/app/models/weatherReport.model';
 import { MatIconRegistry } from '@angular/material/icon';
 import { SnackbarcontrolService } from '../services/snackbarcontrol.service';
-import { ServerResponse } from './../models/weatherReport.model';
+import { ServerResponse, Area } from './../models/weatherReport.model';
 import { WeatherService } from './../services/weather.service';
 import { Component, DoCheck, OnInit } from '@angular/core';
 import { WeatherToIconService } from '../services/weather-to-icon.service';
@@ -15,24 +16,7 @@ export class WeatherDisplayComponent implements OnInit, DoCheck {
   isReadyForUpdate: boolean = false;
 
   weatherData: ServerResponse = {} as ServerResponse;
-  city = 'City';
-  country = 'Country';
-  station = 'Station';
-  description = 'Description';
-  iconNow = 'partly_cloudy_day';
-  iconToday = 'cloudy';
-  iconTomorrow = 'sunny';
-  currentTemp = '0';
-  feelsLike = '0';
-  humidity = '0';
-  pressure = '0';
-  visibility = '0';
-  uvIndex = '0';
-  windDirection = 'XXX';
-  windSpeed = '0';
-  maxTemp = '0';
-  minTemp = '0';
-  precip = '0';
+  conditionNow = new CurrentConditionUIModel();
   forecast = [] as Weather[];
 
   constructor(
@@ -49,36 +33,67 @@ export class WeatherDisplayComponent implements OnInit, DoCheck {
     this.updateDisplay();
   }
   ngDoCheck(): void {
-    if (this.isReadyForUpdate != this.weatherService.isReadyForUpdate) {
-      this.isReadyForUpdate = this.weatherService.isReadyForUpdate;
+    if (this.updateHasBeenCalled()) {      
       this.updateDisplay();
     }
   }
+  private updateHasBeenCalled() {
+    return this.isReadyForUpdate != this.weatherService.isReadyForUpdate;
+  }
+
   updateDisplay() {
+    this.isReadyForUpdate = this.weatherService.isReadyForUpdate;
     this.weatherData = this.weatherService.getWeather();
     if (this.weatherService.isReadyForUpdate) {
       this.mapData();
-      this.iconNow = this.wtiService.getIcon(this.description);
+      var descriptionForIcon = this.conditionNow.description;
+      this.conditionNow.icon = this.wtiService.getIcon(descriptionForIcon);
     }
   }
 
   mapData() {
-    this.city = this.weatherData.nearest_area[0].region[0].value;
-    this.station = this.weatherData.nearest_area[0].areaName[0].value;
-    this.country = this.weatherData.nearest_area[0].country[0].value;
-    this.description =
-      this.weatherData.current_condition[0].weatherDesc[0].value;
-    this.currentTemp = this.weatherData.current_condition[0].temp_C;
-    this.feelsLike = this.weatherData.current_condition[0].FeelsLikeC;
-    this.humidity = this.weatherData.current_condition[0].humidity;
-    this.pressure = this.weatherData.current_condition[0].pressure;
-    this.visibility = this.weatherData.current_condition[0].visibility;
-    this.uvIndex = this.weatherData.current_condition[0].uvIndex;
-    this.windDirection = this.weatherData.current_condition[0].winddir16Point;
-    this.windSpeed = this.weatherData.current_condition[0].windspeedKmph;
-    this.maxTemp = this.weatherData.weather[0].maxtempC;
-    this.minTemp = this.weatherData.weather[0].mintempC;
-    this.precip = this.weatherData.current_condition[0].precipMM;
+    var recievedArea = this.weatherData.nearest_area[0];
+    this.mapAreaData(recievedArea);
+
+    var recievedCondition = this.weatherData.current_condition[0];
+    this.mapConditionData(recievedCondition);
+
+    var recievedWeather = this.weatherData.weather[0];
+    this.mapWeatherData(recievedWeather);
+
     this.forecast = this.weatherData.weather;
+  }
+
+  mapAreaData(recievedArea: Area) {
+    var recievedCity = recievedArea.areaName[0].value;
+    this.conditionNow.city = recievedCity;
+
+    var recievedStation = recievedArea.region[0].value;
+    this.conditionNow.station = recievedStation;
+    
+    var recievedCountry = recievedArea.country[0].value;
+    this.conditionNow.country = recievedCountry;
+  }
+
+  mapConditionData(recievedCondition: CurrentConditionData) {
+    this.conditionNow.currentTemp = recievedCondition.temp_C;
+    this.conditionNow.feelsLike = recievedCondition.FeelsLikeC;
+
+    this.conditionNow.humidity = recievedCondition.humidity;
+    this.conditionNow.pressure = recievedCondition.pressure;
+    
+    this.conditionNow.windDirection = recievedCondition.winddir16Point;
+    this.conditionNow.windSpeed = recievedCondition.windspeedKmph;
+
+    this.conditionNow.precipitationLevel = recievedCondition.precipMM;
+    this.conditionNow.uvIndex = recievedCondition.uvIndex;
+    this.conditionNow.visibility = recievedCondition.visibility;
+
+    var recievedDescription = recievedCondition.weatherDesc[0].value;
+    this.conditionNow.description = recievedDescription;
+  }
+  mapWeatherData(recievedWeather: Weather) {
+    this.conditionNow.maxTemp = recievedWeather.maxtempC;
+    this.conditionNow.minTemp = recievedWeather.mintempC;
   }
 }
