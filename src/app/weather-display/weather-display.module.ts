2a6546f4ce01769b5267a0943edd7e6e019088b9
Jaciel Israel Resendiz Ochoa <jresendizochoa@deloitte.com>
2023-02-09 18:15:27 -0600

    Implements custom Pipes to weatherModule

--- src/app/weather-display/weather-display.module.ts
+++ src/app/weather-display/weather-display.module.ts
@@ -1,3 +1,4 @@
+import { MatButtonModule } from '@angular/material/button';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { MatCardModule } from '@angular/material/card';
 import { WeatherDisplayComponent } from './weather-display.component';
@@ -5,14 +6,25 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ForecastCardComponent } from './forecast-card/forecast-card.component';
 import { CurrentConditionCardComponent } from './current-condition-card/current-condition-card.component';
+import { AppRoutingModule } from '../app-routing.module';
+import { TemperaturePipe } from '../pipes/temperature.pipe';
+import { TwentyFourHourPipe } from '../pipes/twentyfour-hours.pipe';
 
 @NgModule({
-  declarations: [WeatherDisplayComponent, ForecastCardComponent, CurrentConditionCardComponent],
+  declarations: [
+    WeatherDisplayComponent,
+    ForecastCardComponent,
+    CurrentConditionCardComponent,
+    TemperaturePipe,
+    TwentyFourHourPipe
+  ],
   exports: [WeatherDisplayComponent],
   imports: [
     MatCardModule,
     MatProgressSpinnerModule,
-    CommonModule
-  ]
+    MatButtonModule,
+    AppRoutingModule,
+    CommonModule,
+  ],
 })
-export class WeatherDisplayModule { }
+export class WeatherDisplayModule {}
