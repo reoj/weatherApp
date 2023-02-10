621d20f2905c33913267cc41079b1bbaee263143
Jaciel Israel Resendiz Ochoa <jresendizochoa@deloitte.com>
2023-02-09 18:16:58 -0600

    Adds Custom Routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,19 @@
-import { WeatherService } from './services/weather.service';
+import { WeatherDisplayComponent } from './weather-display/weather-display.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { MoonDisplayComponent } from './moon-display/moon-display.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/weather', pathMatch: 'full' },
+  { path: 'weather', component: WeatherDisplayComponent },
+  { path: 'moon', component: MoonDisplayComponent },
+  { path: '**', redirectTo: '/weather' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule {
+
 }
