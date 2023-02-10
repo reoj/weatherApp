e57b6978b524efb6df5cfb70a7841979d5bf0d00
Jaciel Israel Resendiz Ochoa <jresendizochoa@deloitte.com>
2023-02-10 15:18:07 -0600

    Binds buttonChecked to Current Route

--- src/app/weather-display/display.component.ts
+++ src/app/weather-display/display.component.ts
@@ -1,15 +1,16 @@
 import { WeatherService } from '../services/weather.service';
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-display',
   templateUrl: './display.component.html',
   styleUrls: ['./display.component.css'],
-  
 })
 export class WeatherDisplayComponent {
-  weatherService: WeatherService;
-  constructor(public service: WeatherService) {
+  public weatherService: WeatherService;
+  public activeRoute = '/';
+  constructor(public service: WeatherService, public router: Router) {
     this.weatherService = service;
   }
 }
