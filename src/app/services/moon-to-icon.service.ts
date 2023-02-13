638d4ceef12b3385ad1f3bc80baa5e8985c6a55e
Jaciel Israel Resendiz Ochoa <jresendizochoa@deloitte.com>
2023-02-13 11:02:46 -0600

    Implements MoonToIcon Service

---
+++ src/app/services/moon-to-icon.service.ts
@@ -0,0 +1,32 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class MoonToIconService {
+
+  constructor() { }
+
+  getMoonPhaseIcon(phaseName: string) {
+    switch (phaseName) {
+      case 'New Moon':
+        return 'nf-md-moon_new';
+      case 'Waxing Crescent':
+        return 'nf-md-moon_waxing_crescent';
+      case 'First Quarter':
+        return 'nf-md-moon_first_quarter';
+      case 'Waxing Gibbous':
+        return 'nf-md-moon_waxing_gibbous';
+      case 'Full Moon':
+        return 'nf-md-moon_full';
+      case 'Waning Gibbous':
+        return 'nf-md-moon_waning_gibbous';
+      case 'Last Quarter':
+        return 'nf-md-moon_last_quarter';
+      case 'Waning Crescent':
+        return 'nf-md-moon_waning_crescent';
+      default:
+        return 'nf-md-moon_new';
+    }
+  }
+}
