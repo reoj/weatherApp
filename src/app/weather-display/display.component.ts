2a13f1f5138fa1d6a4d57091a1d4e7d986fd1d5e
Jaciel Israel Resendiz Ochoa <jresendizochoa@deloitte.com>
2023-02-21 13:25:48 -0600

    Fixes Display component to be a Parent Route

--- src/app/weather-display/display.component.ts
+++ src/app/weather-display/display.component.ts
@@ -1,18 +1,21 @@
 import { WeatherService } from '../services/weather.service';
 import { Component, OnDestroy, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
-import { FadeFromUnderAnimation } from './animations/fade.animation';
+import { Router, ActivatedRoute } from '@angular/router';
+import { FadeFromUnderAnimation } from '../animations/fade.animation';
 
 @Component({
   selector: 'app-display',
   templateUrl: './display.component.html',
   styleUrls: ['./display.component.css'],
+  host: {
+    class: 'parent-container',
+  },
   animations: [
     FadeFromUnderAnimation,]
 })
 export class WeatherDisplayComponent implements OnInit, OnDestroy{
   public weatherService: WeatherService;
-  public activeRoute = '/';
+  public activeRoute = '/main';
 
   public animationState = 'void';
   constructor(public service: WeatherService, public router: Router) {
@@ -22,15 +25,18 @@ export class WeatherDisplayComponent implements OnInit, OnDestroy{
     this.router.events.subscribe((val) => {
       this.activeRoute = this.router.url;
     });
+    if (this.weatherService.hasWeather) {
+      this.router.navigate(['/main/weather']);
+    }
     this.animationState = 'in';
   }
   ngOnDestroy(): void {
     this.animationState = 'void';
   }
   public checkIsRouteWeather(): boolean {
-    return this.activeRoute === '/' || this.activeRoute === '/weather'
+    return this.activeRoute === '/main' || this.activeRoute === '/main/weather'
   }
   public checkIsRouteMoon(): boolean { 
-    return this.activeRoute === '/moon'
+    return this.activeRoute === '/main/moon'
   }
 }
