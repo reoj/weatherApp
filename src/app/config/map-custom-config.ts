43f2638e92cf117ce4baf082dfb714f690d3cb3b
Jaciel Israel Resendiz Ochoa <jresendizochoa@deloitte.com>
2023-02-23 15:35:33 -0600

    Implements proper donfiguration map

---
+++ src/app/config/map-custom-config.ts
@@ -0,0 +1,6 @@
+export const GLOBAL_CONFIG = {
+    api: 'https://wttr.in/',
+    debug_mode: true,
+    format_param_key: 'format',
+    format_param_value: 'j1'
+}
--- src/app/config/mock-custom-config.ts
+++
@@ -1,4 +0,0 @@
-export const global_config = {
-    api: 'https://wttr.in/',
-    debug_mode: true
-}
